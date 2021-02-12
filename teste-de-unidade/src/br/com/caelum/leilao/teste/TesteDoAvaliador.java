61749444b459078ddc8d0fa17c586d24f30312ff
304867172 <nogueira@pm.ba.gov.br>
2021-02-12 09:41:50 -0300

    Primeiro teste junit

---
+++ teste-de-unidade/src/br/com/caelum/leilao/teste/TesteDoAvaliador.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package br.com.caelum.leilao.teste;
+
+import org.junit.Test;
+
+import br.com.caelum.leilao.dominio.Lance;
+import br.com.caelum.leilao.dominio.Leilao;
+import br.com.caelum.leilao.dominio.Usuario;
+import br.com.caelum.leilao.servico.Avaliador;
+import junit.framework.Assert;
+
+
+/**
+ * @author andersonnogueira
+ * @since Feb 12, 2021
+ */
+public class TesteDoAvaliador {
+
+	@Test
+    public void deveEntenderLancesEmOrdemCrescente() {
+        Usuario joao = new Usuario("João");
+        Usuario jose = new Usuario("José");
+        Usuario maria = new Usuario("Maria");
+
+        Leilao leilao = new Leilao("Playstation 3 Novo");
+
+        leilao.propoe(new Lance(joao, 300.0));
+        leilao.propoe(new Lance(jose, 400.0));
+        leilao.propoe(new Lance(maria, 250.0));
+
+        Avaliador leiloeiro = new Avaliador();
+        leiloeiro.avalia(leilao);
+        
+        double maiorEsperado = 400;
+        double menorEsperado = 250;
+
+        Assert.assertEquals(maiorEsperado, leiloeiro.getMaiorLance(), 0.00001);
+        Assert.assertEquals(menorEsperado, leiloeiro.getMenorLance(), 0.00001);
+    }
+}
