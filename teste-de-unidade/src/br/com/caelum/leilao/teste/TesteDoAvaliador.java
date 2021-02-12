53bf2f887387a0d2bcd9854e80e890dc9c865915
304867172 <nogueira@pm.ba.gov.br>
2021-02-12 10:46:04 -0300

    Comparando os 3 maiores valores em uma lista

--- teste-de-unidade/src/br/com/caelum/leilao/teste/TesteDoAvaliador.java
+++ teste-de-unidade/src/br/com/caelum/leilao/teste/TesteDoAvaliador.java
@@ -5,6 +5,8 @@ package br.com.caelum.leilao.teste;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
+
 import org.junit.Test;
 
 import br.com.caelum.leilao.dominio.Lance;
@@ -40,4 +42,27 @@ public class TesteDoAvaliador {
         assertEquals(maiorEsperado, leiloeiro.getMaiorLance(), 0.00001);
         assertEquals(menorEsperado, leiloeiro.getMenorLance(), 0.00001);
     }
+	@Test
+	public void deveEncontrarOsTresMaioresLances() {
+		Usuario joao = new Usuario("João");
+		Usuario jose = new Usuario("José");
+		Usuario maria = new Usuario("Maria");
+		
+		Leilao leilao = new Leilao("Playstation 3 Novo");
+		
+		leilao.propoe(new Lance(maria, 250.0));
+		leilao.propoe(new Lance(jose, 270.0));
+		leilao.propoe(new Lance(joao, 300.0));
+		leilao.propoe(new Lance(jose, 400.0));
+		
+		Avaliador leiloeiro = new Avaliador();
+		leiloeiro.avalia(leilao);
+		
+		List<Lance> maiores = leiloeiro.getTresMaiores();
+		
+		assertEquals(3, maiores.size());
+		assertEquals(400, maiores.get(0).getValor(), 0.00001);
+		assertEquals(300, maiores.get(1).getValor(), 0.00001);
+		assertEquals(270, maiores.get(2).getValor(), 0.00001);
+	}
 }
